26ca61f84f3ab5220d10faf0a90a7610aa2b1f55
Visnu Pitiyanuvath <visnup@gmail.com>
2022-09-17 01:33:41 -0700

    Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,9 @@ function Community() {
   return (
     <div>
       <p className={styles.description}>
-        {dealt?.community!.map((c: string) => <div key={c}>{c}</div>)}
+        {dealt?.community!.map((c: string) => (
+          <div key={c}>{c}</div>
+        ))}
       </p>
       <button className={styles.button} onClick={() => deal()}>
         Deal!
@@ -19,20 +21,16 @@ function Community() {
   )
 }
 
-function Hole({player}) {
+function Hole({ player }) {
   const dealt = useQuery('getDealt')
   const cards = dealt?.cards.slice(player.n, player.n + 2)
-  return (
-    <div>
-      {JSON.stringify(cards)}
-    </div>
-  )
+  return <div>{JSON.stringify(cards)}</div>
 }
 
 const Home: NextPage = () => {
   const join = useMutation('join')
   const ping = useMutation('ping')
-  const [player, setPlayer] = useState();
+  const [player, setPlayer] = useState()
 
   useEffect(() => {
     if (!player) join().then(setPlayer)
@@ -46,11 +44,19 @@ const Home: NextPage = () => {
 
   return (
     <div className={styles.container}>
-      <Head><title>Poker</title></Head>
+      <Head>
+        <title>Poker</title>
+      </Head>
 
       <main className={styles.main}>
         {JSON.stringify(player)}
-        {player ? player.n === 0 ? <Community /> : <Hole player={player} /> : null}
+        {player ? (
+          player.n === 0 ? (
+            <Community />
+          ) : (
+            <Hole player={player} />
+          )
+        ) : null}
       </main>
     </div>
   )
