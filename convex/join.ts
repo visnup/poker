24248f7736360ca662a7f1331c30f11cb6cd0430
Visnu Pitiyanuvath <visnup@gmail.com>
2022-09-17 01:33:38 -0700

    Clean up old players

--- convex/join.ts
+++ convex/join.ts
@@ -3,10 +3,14 @@ import { mutation } from './_generated/server.js'
 export default mutation(async ({ db }) => {
   const now = Date.now()
   const recent = now - 10e3
-  const players = await db
+
+  const stale = await db
     .table('players')
-    .filter((q) => q.gte(q.field('t'), recent))
+    .filter((q) => q.lt(q.field('t'), recent))
     .collect()
+  for (const { _id } of stale) await db.delete(_id)
+
+  const players = await db.table('players').collect()
   for (let n = 0; n < 10; n++)
     if (!players[n] || n < players[n].n) {
       const player = { n, t: now }
