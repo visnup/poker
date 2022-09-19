bb32af4383cc4a78a4245b5e38bfc64886a50719
Visnu Pitiyanuvath <visnup@gmail.com>
2022-09-19 00:31:50 -0700

    More @use-gesture

--- src/components/Hole.tsx
+++ src/components/Hole.tsx
@@ -1,18 +1,30 @@
+import { useGesture } from "@use-gesture/react";
 import { useEffect, useState } from "react";
+import { animated, useSpring, config } from "react-spring";
 import { useQuery } from "../../convex/_generated/react";
 import { Card } from "./Card";
-import { useDrag } from "./useDrag";
 
 export function Hole({ seat }: { seat: number }) {
   const dealt = useQuery("getDealt");
-  const [distance, direction] = useDrag();
   const [rotation, setRotation] = useState(() => Math.random() * 10 - 5);
   useEffect(() => setRotation(Math.random() * 10 - 5), [dealt]);
+  const [styles, api] = useSpring(() => ({
+    clipPath: "circle(0% at 10px 20px)",
+    config: config.stiff,
+  }));
+  const bind = useGesture({
+    onDragStart: () => setRotation(2),
+    onDragEnd: () => setRotation(Math.random()),
+    onDrag: ({ down, movement }) => {
+      const width = down ? Math.hypot(...movement) : 0;
+      api.start({ clipPath: `circle(${width}px at 10px 20px)` });
+    },
+  });
 
   const cards = dealt?.cards.slice(seat, seat + 2) ?? [];
 
   return (
-    <div className="cards">
+    <div className="cards" {...bind()}>
       <div className="layer">
         {cards.map((c, i) => (
           <div className="placement" key={c}>
@@ -20,20 +32,19 @@ export function Hole({ seat }: { seat: number }) {
           </div>
         ))}
       </div>
-      <div
-        className="layer"
-        style={{ clipPath: `circle(${distance}% at 40px 30px)` }}
-      >
-        {cards.map((c, i) => (
-          <div className="placement" key={c}>
-            <Card
-              card={c}
-              anchor="top"
-              rotation={i ? rotation : -rotation}
-              revealed
-            />
-          </div>
-        ))}
+      <div className="layer">
+        <animated.div style={styles}>
+          {cards.map((c, i) => (
+            <div className="placement" key={c}>
+              <Card
+                card={c}
+                anchor="top"
+                rotation={i ? rotation : -rotation}
+                revealed
+              />
+            </div>
+          ))}
+        </animated.div>
       </div>
       <style jsx>
         {`
@@ -41,6 +52,7 @@ export function Hole({ seat }: { seat: number }) {
             position: relative;
             min-width: 325px;
             min-height: 100vh;
+            touch-action: none;
           }
           .layer {
             position: absolute;
