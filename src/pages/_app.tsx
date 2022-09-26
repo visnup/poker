47ac1b4ae9caf31dab50e5a21c48274243f16e68
Visnu Pitiyanuvath <visnup@gmail.com>
2022-09-25 23:14:17 -0700

    Ignore unsaved changes warning

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,9 @@ import type { AppProps } from "next/app";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import clientConfig from "../../convex/_generated/clientConfig";
 import Head from "next/head";
-const convex = new ConvexReactClient(clientConfig);
+const convex = new ConvexReactClient(clientConfig, {
+  unsavedChangesWarning: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
